b598093024de0656a703704281b7ec210be83006
Hardik <hardik.vaghasia@gmail.com>
2020-03-16 18:56:14 +0000

    First commit

---
+++ FirebaseWrapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseWrapper
+//
+//  Created by Hardik Vaghasia on 16/03/2020.
+//  Copyright © 2020 Hardik Vaghasia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
